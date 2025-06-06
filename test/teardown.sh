1d85a1eb1bbc9f51c1d299b578adf2349a1c41ef
Erik Stromdahl <erik.stromdahl@gmail.com>
2025-06-06 16:26:16 +0200

    test: add PID file for mosquitto broker
    Instead of killing all mosquitto instances, only the instance started by
    the test script is killed.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- test/teardown.sh
+++ test/teardown.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
 
+# kill all mqtt-forward instances
 killall mqtt-forward
-killall mosquitto
+
+# Kill mosquitto broker
+MOSQUITTO_PID_FILE="/tmp/mosquitto-mqtt-forward-test.pid"
+
+if [[ -f "${MOSQUITTO_PID_FILE}" ]]; then
+    PID=$(cat "${MOSQUITTO_PID_FILE}")
+    if kill -0 "${PID}" 2>/dev/null; then
+        kill "${PID}"
+        echo "Mosquitto (PID ${PID}) terminated."
+        rm "${MOSQUITTO_PID_FILE}"
+    else
+        echo "Process with PID ${PID} is not running."
+        rm "${MOSQUITTO_PID_FILE}"
+    fi
+else
+    echo "Mosquitto broker PID file not found."
+fi
